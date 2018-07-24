300c3957cc711b9b11c52e021d3c442b068a8ede
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2018-07-24 18:28:19 +0300

    FE: add indexes, udf and uda

--- forward_engineering/api.js
+++ forward_engineering/api.js
@@ -3,13 +3,14 @@
 const { tab } = require('./helpers/formatHelper'); 
 const { getTableStatement } = require('./helpers/tableHelper');
 const { getUserDefinedTypes } = require('./helpers/udtHelper');
+const { getIndexes } = require('./helpers/indexHelper');
 
 module.exports = {
 	generateScript(data, logger, callback) {
 		data.jsonSchema = JSON.parse(data.jsonSchema);
 		data.modelDefinitions = JSON.parse(data.modelDefinitions);
 		data.internalDefinitions = JSON.parse(data.internalDefinitions);
-		let result = JSON.stringify(data, null, 2);
+		// let result = JSON.stringify(data, null, 2);
 		const keyspace = getKeyspaceStatement(data.containerData[0]);
 		const modelUdt = getUserDefinedTypes(data.containerData[0].name, data.modelDefinitions);
 		const internalUdt = getUserDefinedTypes(data.containerData[0].name, data.internalDefinitions);
@@ -20,8 +21,15 @@ module.exports = {
 			modelDefinitions: data.modelDefinitions,
 			internalDefinition: data.internalDefinitions
 		});
+		const indexes = getIndexes((data.entityData[1].SecIndxs || []), [
+			data.jsonSchema,
+			data.modelDefinitions,
+			data.internalDefinitions
+		], data.entityData[0].collectionName, data.containerData[0].name);
+		const UDF = getUserDefinedFunctions(data.containerData[1].UDFs);
+		const UDA = getUserDefinedAggregations(data.containerData[2].UDAs);
 
-		callback(null, [keyspace, modelUdt, internalUdt, table, result].join('\n\n\n'));
+		callback(null, [keyspace, modelUdt, internalUdt, table, indexes, UDF, UDA].filter(item => item).join('\n\n'));
 	},
 };
 
@@ -50,4 +58,12 @@ const getKeyspaceStatement = (keyspaceData) => {
 		getReplication(keyspaceData.replStrategy, keyspaceData.replFactor, keyspaceData.dataCenters),
 		getDurableWrites(keyspaceData.durableWrites)
 	);
+};
+
+const getUserDefinedFunctions = (udfItems) => {
+	return udfItems.map(item => item.storedProcFunction).filter(item => item).join('\n');
+};
+
+const getUserDefinedAggregations = (udaItems) => {
+	return udaItems.map(item => item.storedProcFunction).filter(item => item).join('\n');
 };

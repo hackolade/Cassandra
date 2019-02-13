66cc03a2544c01a26b7375af47638fa232af14f2
nikolay <nikolay.datsko@binary-studio.com>
2019-02-13 16:33:03 +0200

    Options handling changed

--- forward_engineering/helpers/updateHelper.js
+++ forward_engineering/helpers/updateHelper.js
@@ -6,21 +6,23 @@ const add = columnData => `ADD "${columnData.name}" ${columnData.type};\n`;
 const getDelete = deleteData => `${alterPrefix(deleteData.tableName, deleteData.keySpace)} ${remove(deleteData.columnData.name)}`;
 const getAdd = addData => `${alterPrefix(addData.tableName, addData.keySpace)} ${add(addData.columnData)}`;
 const getUpdate = updateData => getDelete(updateData) + getAdd(updateData);
-const objectContainProp = (object, key) => object[key] ? true : false;
-const getAnd = data => ` AND ${data.key} = "${data.value}"`
+const objectContainsProp = (object, key) => object[key] ? true : false;
+const getAnd = data => ` AND ${data.key} = '${data.value}'`
 const getChangeOption = changeData => {
+    const newOptions = getComparedOptions(changeData.options.new.split("\nAND "), changeData.options.old.split("\nAND "));
     let alterTableScript = '';
-    const firstModified = changeData.modified.shift();
-    const firstKey = Object.keys(firstModified)[0];
-    const firstValue = firstModified[firstKey];
+    
+    if (changeData.comment) {
+        alterTableScript += `${alterPrefix(changeData.tableName, changeData.keySpace)} WITH comment = '${changeData.comment}'`;
+    } else {
+        alterTableScript += `${alterPrefix(changeData.tableName, changeData.keySpace)} WITH ${firstKey} = '${firstValue}'`;
+    }
 
-    alterTableScript += `${alterPrefix(changeData.tableName, changeData.keySpace)} WITH ${firstKey} = '${firstValue}'`;
-
-    if (changeData.modified.length) {
+    if (!newOptions.length) {
         return alterTableScript += ';\n\n';
     }
 
-    alterTableScript += '\n' + changeData.modified.map((element) => {
+    alterTableScript += '\n' + newOptions.map((element) => {
         const key = Object.keys(element)[0];
         const value = element[key];
         return getAnd({ key, value });
@@ -29,14 +31,33 @@ const getChangeOption = changeData => {
     return alterTableScript += ';\n\n';
 };
 
+const getComparedOptions = (oldOptions, newOptions) => {
+    const newOptionsDIf = newOptions.filter(newOption => {
+        const inOld = oldOptions.filter((oldOption) => {
+            return (newOption === oldOption);
+        });
+
+        return inOld.length ? false : true;
+    });
+
+
+
+    return newOptionsDIf.map( (option) => {
+        const key = option.split(' = ')[0];
+        const value = option.split(' = ')[1];
+
+        return ({ [key]: value });
+    });
+}
+
 const handleChange = (child, udtMap, generator, data) => {
     let alterTableScript = '';
 
-    if (objectContainProp(child, 'items') && child.items.length) {
+    if (objectContainsProp(child, 'items') && child.items.length) {
         child.items.forEach(element => {
             alterTableScript += handleItem(element, udtMap, generator, data);
         });
-    } else if (objectContainProp(child, 'items')) {
+    } else if (objectContainsProp(child, 'items')) {
         alterTableScript += handleItem(child.items, udtMap, generator, data);
     }
 
@@ -50,11 +71,12 @@ const handleOptions = (generator, itemCompModData, tableName) => {
         return alterTableScript;
     }
 
-    if (itemCompModData.modified) {
+    if (itemCompModData.options) {
         alterTableScript += getChangeOption({
             keySpace: itemCompModData.keyspaceName,
             tableName: tableName,
-            modified: itemCompModData.modified
+            options: itemCompModData.options,
+            comment: itemCompModData.comment
         });
     }
 
@@ -64,85 +86,87 @@ const handleOptions = (generator, itemCompModData, tableName) => {
 const handleItem = (item, udtMap, generator, data) => {
     let alterTableScript = '';
 
-    if (!objectContainProp(item, 'properties')) {
+    if (!objectContainsProp(item, 'properties')) {
         return alterTableScript;
     }
 
     const itemProperties = item.properties;
 
-    alterTableScript += Object.keys(itemProperties).reduce((alterTableScript, tableName) => {
-        const itemCompModData = itemProperties[tableName].role.compMod;
+    alterTableScript += Object.keys(itemProperties)
+        .reduce((alterTableScript, tableName) => {
+            const itemCompModData = itemProperties[tableName].role.compMod;
 
-        if (!itemCompModData) {
+            if (!itemCompModData) {
+                return alterTableScript;
+            }
+
+            const tableProperties = item.properties[tableName].properties;
+
+            alterTableScript += handleOptions(generator, itemCompModData, tableName);
+
+            if (itemCompModData.deleted) {
+                return alterTableScript;
+            }
+
+            if (!tableProperties) {
+                return alterTableScript;
+            }
+
+            alterTableScript += handlePropeties({ generator, tableProperties, udtMap, itemCompModData, tableName });
+
             return alterTableScript;
-        }
-
-        const tableProperties = item.properties[tableName].properties;
-
-        alterTableScript += handleOptions(generator, itemCompModData, tableName);
-
-        if (itemCompModData.deleted) {
-            return alterTableScript;
-        }
-
-        if (!tableProperties) {
-            return alterTableScript;
-        }
-
-        alterTableScript += handlePropeties({ generator, tableProperties, udtMap, itemCompModData, tableName });
-
-        return alterTableScript;
     }, '');
 
     return alterTableScript;
 }
 
 const handlePropeties = ({ generator, tableProperties, udtMap, itemCompModData, tableName }) => {
-    return Object.keys(tableProperties).reduce((alterTableScript, columnName) => {
-        const columnData = getTypeByData(tableProperties[columnName], udtMap, columnName);
-        let keyspaceName;
+    return Object.keys(tableProperties)
+        .reduce((alterTableScript, columnName) => {
+            const columnData = getTypeByData(tableProperties[columnName], udtMap, columnName);
+            let keyspaceName;
 
-        if (itemCompModData && itemCompModData.keyspaceName) {
-            keyspaceName = itemCompModData.keyspaceName;
-        };
+            if (itemCompModData && itemCompModData.keyspaceName) {
+                keyspaceName = itemCompModData.keyspaceName; 
+            };
+        
+            alterTableScript += generator({
+                keySpace: keyspaceName,
+                tableName: tableName,
+                columnData: {
+                    name: columnName,
+                    type: columnData
+                }
+            });
 
-        alterTableScript += generator({
-            keySpace: keyspaceName,
-            tableName: tableName,
-            columnData: {
-                name: columnName,
-                type: columnData
-            }
-        });
-
-        return alterTableScript;
+            return alterTableScript;
     }, '');
 }
 
 const getAlterTableScript = (child, udtMap, data) => {
     let alterTableScript = '';
 
-    if (objectContainProp(child, 'properties')) {
+    if (objectContainsProp(child, 'properties')) {
         alterTableScript += getAlterTableScript(child.properties, udtMap, data);
     }
 
-    if (objectContainProp(child, 'items')) {
+    if (objectContainsProp(child, 'items')) {
         alterTableScript += getAlterTableScript(child.items, udtMap, data);
     }
 
-    if (objectContainProp(child, 'entities')) {
+    if (objectContainsProp(child, 'entities')) {
         alterTableScript += getAlterTableScript(child.entities, udtMap, data);
     }
 
-    if (objectContainProp(child, 'modified')) {
+    if (objectContainsProp(child, 'modified')) {
         alterTableScript += handleChange(child.modified, udtMap, getUpdate, data);
     }
 
-    if (objectContainProp(child, 'deleted')) {
+    if (objectContainsProp(child, 'deleted')) {
         alterTableScript += handleChange(child.deleted, udtMap, getDelete, data);
     }
 
-    if (objectContainProp(child, 'added')) {
+    if (objectContainsProp(child, 'added')) {
         alterTableScript += handleChange(child.added, udtMap, getAdd, data);
     }
 

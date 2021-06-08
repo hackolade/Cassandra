2ead69c3415c704498477b8e39d43d08bde6f83a
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2021-06-08 15:20:42 +0300

    add fe custom and search indexes

--- forward_engineering/helpers/generalHelper.js
+++ forward_engineering/helpers/generalHelper.js
@@ -21,7 +21,25 @@ const retrieveEntityName = (entityConfig) => retrivePropertyFromConfig(
 );
 const retrieveUDF = (containerConfig) => retrivePropertyFromConfig(containerConfig, 1, "UDFs", []);
 const retrieveUDA = (containerConfig) => retrivePropertyFromConfig(containerConfig, 2, "UDAs", []);
-const retrieveIndexes = (entityConfig) => retrivePropertyFromConfig(entityConfig, 1, "SecIndxs", []);
+const retrieveIndexes = (entityConfig) => {
+	const indexTab = entityConfig[1];
+
+	if (!indexTab.searchIndex) {
+		return {
+			indexes: retrivePropertyFromConfig(entityConfig, 1, "SecIndxs", []),
+		};
+	}
+	
+	return {
+		searchIndex: {
+			indexType: 'search',
+			columns: indexTab.searchIndexColumns,
+			config: indexTab.searchIndexConfig,
+			profiles: [indexTab.searchIndexProfiles].filter(Boolean),
+			options: indexTab.searchIndexOptions,
+		},
+	};
+};
 const getTableNameStatement = (keyspaceName, tableName) => getNameWithKeyspace(keyspaceName, `"${tableName}"`);
 const getNameWithKeyspace = (keyspaceName, name) => `${(keyspaceName) ? `"${keyspaceName}".` : ""}${name}`;
 
@@ -136,7 +154,15 @@ const commentDeactivatedStatement = (statement, isActivated = true, isParentActi
 	return useMultiLineComment ? multiLineComment(statement) : insertBeforeEachLine(statement);
 }
 
-const retrieveIsItemActivated = (itemConfig) => retrivePropertyFromConfig(itemConfig, 0, "isActivated", true);
+const retrieveIsItemActivated = (itemConfig) => {
+	const value = retrivePropertyFromConfig(itemConfig, 0, "isActivated");
+
+	if (value === undefined) {
+		return true;
+	}
+
+	return value;
+};
 
 module.exports = {
 	tab,
--- forward_engineering/helpers/indexHelper.js
+++ forward_engineering/helpers/indexHelper.js
@@ -1,36 +1,124 @@
 'use strict'
 
-const { commentDeactivatedStatement } = require('./commentsHelper');
+const { commentDeactivatedStatement, inlineComment } = require('./commentsHelper');
 const { tab, getTableNameStatement } = require('./generalHelper');
 const { getNamesByIds } = require('./schemaHelper');
 
-const getIndexes = (indexes, dataSources, tableName, keyspaceName, isTableActivated, isKeyspaceActivated) => {
-	const indexStatements = unwindIndexes(indexes).map(index => {
-		const isIndexKeyActivated = isIndexColumnKeyActivated(index.SecIndxKey, dataSources);
-		const indexStatement = getIndex(
-			index.name,
-			keyspaceName,
-			tableName,
-			getIndexColumnStatement(index.SecIndxKey, dataSources)
-		);
+const getIndexes = (indexData, dataSources, tableName, keyspaceName, isTableActivated, isKeyspaceActivated) => {
+	const tableNameStatement = getTableNameStatement(keyspaceName, tableName);
+	const isParentActivated = isTableActivated && isKeyspaceActivated;
+	const generalIndexes = getGeneralIndexes(tableNameStatement, dataSources, indexData.indexes);
+	const searchIndex = getSearchIndexStatement(tableNameStatement, dataSources, isParentActivated, indexData.searchIndex);	
+	
+	const indexStatements = [...generalIndexes, ...searchIndex].map(index => {
 		return commentDeactivatedStatement(
-			indexStatement,
-			isIndexKeyActivated,
-			isTableActivated && isKeyspaceActivated
+			index.statement,
+			index.isActivated,
+			isParentActivated
 		);
 	}).join('\n\n');
 
-	return commentDeactivatedStatement(indexStatements, isTableActivated, isKeyspaceActivated);
+	return indexStatements;
 };
 
-const getIndex = (name, keyspaceName, tableName, indexColumnStatement) => (
-	`CREATE INDEX IF NOT EXISTS ${name ? `"${name}"` : ``}\n${tab(`ON ${getTableNameStatement(keyspaceName, tableName)} (${indexColumnStatement});`)}`	
+const getGeneralIndexes = (tableNameStatement, dataSources, indexes = []) => {
+	return unwindIndexes(indexes).map(index => {
+		let indexStatement;
+
+		const isIndexKeyActivated = isIndexColumnKeyActivated(index.SecIndxKey, dataSources);
+		const columnStatement = getIndexColumnStatement(index.SecIndxKey, dataSources);
+
+		if (index.indexType === 'custom') {
+			indexStatement = getCustomIndex({
+				name: index.name,
+				tableName: tableNameStatement,
+				column: columnStatement,
+				using: 'StorageAttachedIndex',
+				options: getCustomOptions(index.customOptions),
+			});
+		} else {
+			indexStatement = getIndex(
+				index.name,
+				tableNameStatement,
+				columnStatement,
+			);
+		}
+
+		return {
+			isActivated: isIndexKeyActivated,
+			statement: indexStatement,
+		};
+	});
+};
+
+const getSearchIndexStatement = (tableNameStatement, dataSources, isParentActivated, searchIndex) => {
+	if (!searchIndex) {
+		return [];
+	}
+
+	const columns = uniqueByName(searchIndex.columns.map(column => ({
+		...column,
+		name: getIndexColumnStatement(column.key[0], dataSources),
+		isActivated: isIndexColumnKeyActivated(column.key[0], dataSources),
+	})));
+	const activatedColumns = columns.filter(column => column.isActivated);
+	const isActivated = activatedColumns.length > 0;
+	const statement = getSearchIndex({
+		tableName: tableNameStatement,
+		isActivated: isActivated && isParentActivated,
+		index: searchIndex,
+		columns,
+	});
+
+	return [{
+		isActivated,
+		statement,
+	}];
+};
+
+const uniqueByName = (columns) => {
+	return columns.reduce((result, column) => {
+		const exists = result.find(c => c.name === column.name);
+
+		if (exists) {
+			return result;
+		}
+
+		return result.concat(column);
+	}, []);
+};
+
+const getIndex = (name, tableName, indexColumnStatement) => (
+	`CREATE INDEX IF NOT EXISTS ${name ? `"${name}"` : ``}\n${tab(`ON ${tableName} (${indexColumnStatement});`)}`	
+);
+
+const getCustomIndex = ({ name, tableName, column, using, options }) => (
+	`CREATE CUSTOM INDEX IF NOT EXISTS ${name ? `"${name}"` : ``}\n` +
+	`${tab(`ON ${tableName} (${column})`)}\n` +
+	`${tab(`USING '${using || 'StorageAttachedIndex'}'`)}` +
+	`${Object.keys(options).length ? '\n' + tab(`WITH OPTIONS = {\n${tab(serializeOptions(options).join(',\n'))}\n}`) : ''};`	
 );
 
 const getIndexColumnStatement = (key, dataSources) => {
 	const { name } = getNamesByIds([key.keyId], dataSources)[key.keyId];
 
-	return `"${name}"`;
+	return wrapKey(`"${name}"`, key.type);
+};
+
+const wrapKey = (column, type) => {
+	if (type === 'keys') {
+		return `KEYS(${column})`;
+	}
+
+	if (type === 'values') {
+		return `VALUES(${column})`;
+	}
+
+	if (type === 'entries') {
+		return `ENTRIES(${column})`;
+	}
+
+	return column;
 };
 
 const isIndexColumnKeyActivated = (key, dataSources) => {
@@ -48,6 +136,159 @@ const unwindIndexes = (indexes) => {
 			});
 		})];
 	}, []);
+};
+
+const serializeOptions = (options) => {
+	return Object.keys(options).map(option => `'${option}': '${options[option]}'`);
+};
+
+const getCustomOptions = (options) => {
+	let result = {};
+
+	if (!options) {
+		return result;
+	}
+
+	if (!options.case_sensitive) {
+		result.case_sensitive = 'false';
+	}
+
+	if (options.normalize) {
+		result.normalize = 'true';
+	}
+
+	if (options.ascii) {
+		result.ascii = 'true';
+	}
+
+	return result;
+};
+
+const getSearchIndex = ({ tableName, columns, index, isActivated }) => {
+	const columnsStatement = getSearchIndexColumnStatements(getSearchIndexColumns(columns), isActivated);
+	const profiles = index.profiles.length ? `\n${tab(`AND PROFILES ${index.profiles.join(', ')}`)}` : '';
+	const indexConfig = getSearchIndexConfig(index.config);
+	const config = indexConfig ? `\n${tab(`AND CONFIG ${indexConfig}`)}` : '';
+	const indexOptions = getSearchIndexOptions(index.options);
+	const options = indexOptions ? `\n${tab(`AND OPTIONS ${indexOptions}`)}` : '';
+
+	return `CREATE SEARCH INDEX IF NOT EXISTS ON ${tableName}\n` + 
+		`${tab(`WITH COLUMNS\n${tab(columnsStatement)}`)}` + 
+		profiles + config + options +
+		';';
+};
+
+const getSearchIndexColumns = (columns) => {
+	return columns.map(column => {
+		const options = [];
+
+		if (column.copyField) {
+			options.push(`copyField: true`);
+		}
+
+		if (column.docValues) {
+			options.push(`docValues: true`);
+		}
+
+		if (column.excluded) {
+			options.push(`excluded: true`);
+		}
+
+		if (!column.indexed) {
+			options.push(`indexed: false`);
+		}
+
+		return {
+			statement: `${column.name}${options.length ? ` {${options.join(', ')}}` : ''}`,
+			isActivated: column.isActivated,
+		};
+	});
+};
+
+const getSearchIndexColumnStatements = (columns, isParentActivated) => {
+	const commentedColumns = !isParentActivated ? [] : columns.filter(column => !column.isActivated);
+	const othersColumns = !isParentActivated ? columns : columns.filter(column => column.isActivated);
+	const commented = commentedColumns.length ? commentedColumns.map(({ statement }) => statement).join(',\n').split('\n').map(inlineComment).join('\n') : '';
+	const others = othersColumns.map(({ statement }) => statement).join(',\n');
+
+	if (commented && others) {
+		return commented + ',\n' + others;
+	}
+
+	if (commented) {
+		return commented;
+	}
+
+	return others;
+};
+
+const getSearchIndexConfig = (config) => {
+	const result = [];
+
+	if (config.autoCommitTime !== 10000) {
+		result.push(`autoCommitTime: ${config.autoCommitTime}`);
+	}
+
+	if (config.defaultQueryField) {
+		result.push(`defaultQueryField: '${config.defaultQueryField}'`);
+	}
+
+	if (config.directoryFactoryClass) {
+		result.push(`directoryFactoryClass: '${config.directoryFactoryClass}'`);
+	}
+
+	if (config.directoryFactory) {
+		result.push(`directoryFactory: '${config.directoryFactory}'`);
+	}
+
+	if (config.filterCacheLowWaterMark !== 1024) {
+		result.push(`filterCacheLowWaterMark: ${config.filterCacheLowWaterMark}`);
+	}
+
+	if (config.filterCacheHighWaterMark !== 2048) {
+		result.push(`filterCacheHighWaterMark: ${config.filterCacheHighWaterMark}`);
+	}
+
+	if (config.mergeMaxThreadCount && config.mergeMaxMergeCount) {
+		result.push(`mergeMaxThreadCount: ${config.mergeMaxThreadCount}`);
+		result.push(`mergeMaxMergeCount: ${config.mergeMaxMergeCount}`);
+	}
+
+	if (config.ramBufferSize !== 512) {
+		result.push(`ramBufferSize: ${config.ramBufferSize}`);
+	}
+
+	if (config.realtime) {
+		result.push(`realtime: true`);
+	}
+
+	if (!result.length) {
+		return '';
+	}
+
+	return `{\n${tab(result.join(',\n'))}\n}`;
+};
+
+const getSearchIndexOptions = (options) => {
+	const result = [];
+
+	if (options.recovery) {
+		result.push(`recovery: true`);
+	}
+
+	if (!options.reindex) {
+		result.push(`reindex: false`);
+	}
+
+	if (options.lenient) {
+		result.push(`lenient: true`);
+	}
+
+	if (!result.length) {
+		return '';
+	}
+
+	return `{${result.join(', ')}}`;
 };
 
 module.exports = {

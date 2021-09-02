49fd8bc65dcafb6bdbf2e464460ed40dc7bace89
Volodymyr Lench <volodymyr.lench@binary-studio.com>
2021-09-02 11:00:31 +0300

    fix applying commented script to instance

--- reverse_engineering/cassandraHelper.js
+++ reverse_engineering/cassandraHelper.js
@@ -1032,6 +1032,10 @@ module.exports = (_) => {
 			? recordSamplingSettings.absolute.value
 				: Math.round( count/100 * per);
 	};
+
+	const cleanOutComments = (script) => {
+		return script.replace(/\/\*([\s\S]+?)\*\//, '');
+	};
 	
 	const splitScriptByQueries = (script) => {
 		const findFunctionStatements = (script) => {
@@ -1039,6 +1043,9 @@ module.exports = (_) => {
 				.map(item => item.trim().replace(/(^\$\$|\$\$$)/g, ''));
 		};
 		const getPlaceholder = (n) => `\$__HACKOLADE__PLACEHOLDER_${n}__\$`;
+		
+		script = cleanOutComments(script);
+
 		const functionStatements = findFunctionStatements(script);
 	
 		const queries = functionStatements.reduce((script, func, i) => {

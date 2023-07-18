16514cd2a6e93d10833c775f39d20beb1d8c465e
Danylo Strelets <danylo.strelets@hackolade.com>
2023-07-18 17:55:53 +0300

    Revert "HCK-3553: remove unnecessary statement"
    This reverts commit fc7573b3335f3fa09af0fb8ea5b385faf34787cb.

--- reverse_engineering/cassandraHelper.js
+++ reverse_engineering/cassandraHelper.js
@@ -1056,6 +1056,10 @@ module.exports = (_) => {
 	
 	
 	const getSampleDocSize = (count, recordSamplingSettings) => {
+		if (recordSamplingSettings.active === 'absolute') {
+			return Number(recordSamplingSettings.absolute.value);
+		}
+
 		const limit = Math.ceil((count * recordSamplingSettings.relative.value) / 100);
 
 		return Math.min(limit, recordSamplingSettings.maxValue);

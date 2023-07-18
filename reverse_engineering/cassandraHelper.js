fc7573b3335f3fa09af0fb8ea5b385faf34787cb
Danylo Strelets <danylo.strelets@hackolade.com>
2023-07-18 17:20:29 +0300

    HCK-3553: remove unnecessary statement

--- reverse_engineering/cassandraHelper.js
+++ reverse_engineering/cassandraHelper.js
@@ -1056,10 +1056,6 @@ module.exports = (_) => {
 	
 	
 	const getSampleDocSize = (count, recordSamplingSettings) => {
-		if (recordSamplingSettings.active === 'absolute') {
-			return Number(recordSamplingSettings.absolute.value);
-		}
-
 		const limit = Math.ceil((count * recordSamplingSettings.relative.value) / 100);
 
 		return Math.min(limit, recordSamplingSettings.maxValue);

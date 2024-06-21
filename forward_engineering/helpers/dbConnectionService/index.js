ec04cf59653bf4b1b6c2e38f8400b1ae27ca30e8
Teamcity <teamcity@hackolade.com>
2024-06-21 17:48:08 +0000

    Release: 0.2.1

--- forward_engineering/helpers/dbConnectionService/index.js
+++
@@ -1,8 +0,0 @@
-const cassandra = require('../../../reverse_engineering/cassandraHelper');
-const applyToInstance = require('./applyToInstance.js')(cassandra);
-const testConnection = require('./testConnection.js')(cassandra);
-
-module.exports = {
-	applyToInstance,
-	testConnection
-};

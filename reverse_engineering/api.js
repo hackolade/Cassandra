9a5c62778f510bf3fb633383e787aa9465e37d2f
yevhenii-moroziuk <yevhen.moroziuk@hackolade.com>
2025-05-14 09:45:59 +0300

    HCK-11091: Move the log of the start to the studio

--- reverse_engineering/api.js
+++ reverse_engineering/api.js
@@ -69,8 +69,6 @@ module.exports = {
 	},
 
 	testConnection: function (connectionInfo, logger, cb, app) {
-		logInfo('Test connection', connectionInfo, logger);
-
 		this.connect(
 			connectionInfo,
 			logger,
@@ -92,7 +90,6 @@ module.exports = {
 	getDbCollectionsNames: function (connectionInfo, logger, cb, app) {
 		initPluginConfiguration(connectionInfo.pluginConfiguration, logger);
 
-		logInfo('Retrieving keyspaces and tables information', connectionInfo, logger);
 		const { includeSystemCollection } = connectionInfo;
 		const cassandra = cassandraHelper();
 
@@ -145,8 +142,6 @@ module.exports = {
 		initPluginConfiguration(data.pluginConfiguration, logger);
 
 		const cassandra = cassandraHelper();
-		logger.log('info', data, 'Retrieving schema', data.hiddenKeys);
-
 		const tables = data.collectionData.collections;
 		const keyspacesNames = data.collectionData.dataBaseNames;
 		const includeEmptyCollection = data.includeEmptyCollection;
@@ -281,12 +276,6 @@ module.exports = {
 			},
 		);
 	},
-};
-
-const logInfo = (step, connectionInfo, logger) => {
-	logger.clear();
-	logger.log('info', logHelper.getSystemInfo(connectionInfo.appVersion), step);
-	logger.log('info', connectionInfo, 'connectionInfo', connectionInfo.hiddenKeys);
 };
 
 const progress = (logger, keyspace, table, message) => {
--- reverse_engineering/logHelper.js
+++ reverse_engineering/logHelper.js
@@ -22,36 +22,6 @@ const checkConnection = (host, port, timeout = 5000) =>
 
 const getPluginVersion = () => packageFile.version;
 
-const getSystemInfo = appVersion => {
-	return (
-		'' +
-		`Date: ${new Date()}` +
-		'\n' +
-		`Application version: ${appVersion}` +
-		'\n' +
-		`Plugin version: ${getPluginVersion()}` +
-		'\n\n' +
-		`System information:` +
-		'\n' +
-		` Hostname:  ${os.hostname()}` +
-		'\n' +
-		` Platform:  ${os.platform()} ${os.arch()}` +
-		'\n' +
-		` Release:   ${os.release()}` +
-		'\n' +
-		` Uptime:    ${toTime(os.uptime())}` +
-		'\n' +
-		` Total RAM: ${(os.totalmem() / 1073741824).toFixed(2)} GB` +
-		'\n' +
-		` CPU Model: ${os.cpus()[0].model}` +
-		'\n' +
-		` CPU Clock: ${maxClock(os.cpus())} MHZ` +
-		'\n' +
-		` CPU Cores: ${os.cpus().length} cores` +
-		'\n\n'
-	);
-};
-
 const maxClock = cpus => {
 	return cpus.reduce((highestClock, cpu) => Math.max(highestClock, cpu.speed), 0);
 };
@@ -63,7 +33,6 @@ const toTime = number => {
 };
 
 const logHelper = {
-	getSystemInfo,
 	checkConnection,
 };
 
